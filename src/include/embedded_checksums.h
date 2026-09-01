3460f92cc40bc58acc2da47a008970c02ab88fbe
itswill00 <itswill00@users.noreply.github.com>
2026-09-01 23:45:42 +0700

    fix: implement smart driver detection showing HWC 4x4 or KCAL Sysfs

--- src/include/embedded_checksums.h
+++ src/include/embedded_checksums.h
@@ -9,7 +9,7 @@ typedef struct {
 static const file_checksum_t g_embedded_checksums[] = {
     { "service.sh", "9aac183e13a46a1fd2309db2a3aebe676ffbfe4cc44f070dc53fd9d7ce8e0419" },
     { "module.prop", "2cf91ed48d01f2fe73905e97ef21d277590256075c6d079d2ba91d847d109d9c" },
-    { "webroot/index.html", "8a07850712094e91faa7b2fb05b87192c974a153291e94a33734d5c7d1e08a77" },
+    { "webroot/index.html", "4b317f87aa615bcab61029dd1883abba6cf4e5843a6993c228e28f3e8dcc8080" },
 };
 
 #endif /* EMBEDDED_CHECKSUMS_H */
