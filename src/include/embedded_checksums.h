b59a26fec3e95e2efabc3f01e5e8e6dc40f5da91
itswill00 <itswill00@users.noreply.github.com>
2026-09-01 23:37:57 +0700

    fix: WebUI state bugs, slider debouncing, preset sync, and bridge callback leaks

--- src/include/embedded_checksums.h
+++ src/include/embedded_checksums.h
@@ -9,7 +9,7 @@ typedef struct {
 static const file_checksum_t g_embedded_checksums[] = {
     { "service.sh", "9aac183e13a46a1fd2309db2a3aebe676ffbfe4cc44f070dc53fd9d7ce8e0419" },
     { "module.prop", "2cf91ed48d01f2fe73905e97ef21d277590256075c6d079d2ba91d847d109d9c" },
-    { "webroot/index.html", "85c482dafb5bc37c18c4e01c77f6f276379b9959d9c5f726b4d6eff922acfcd8" },
+    { "webroot/index.html", "9c723e6791256d7399b789564c033c3cd499d7efdcf7f0c7db46c6cb1530336a" },
 };
 
 #endif /* EMBEDDED_CHECKSUMS_H */
