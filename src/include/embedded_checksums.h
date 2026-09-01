1aaeab2f38ca02b9c2b9e56b4ff0d51e983ff5e6
itswill00 <itswill00@users.noreply.github.com>
2026-09-01 23:40:33 +0700

    feat: add developer options refresh rate overlay toggle and fix refresh rate parser

--- src/include/embedded_checksums.h
+++ src/include/embedded_checksums.h
@@ -9,7 +9,7 @@ typedef struct {
 static const file_checksum_t g_embedded_checksums[] = {
     { "service.sh", "9aac183e13a46a1fd2309db2a3aebe676ffbfe4cc44f070dc53fd9d7ce8e0419" },
     { "module.prop", "2cf91ed48d01f2fe73905e97ef21d277590256075c6d079d2ba91d847d109d9c" },
-    { "webroot/index.html", "6271880bf105ffa8b2230821ded9969a8cef19763536092e85646109e07f2d4b" },
+    { "webroot/index.html", "10d96004c807da218c09fbf6217a110c6d858b01959c2fcc0def779a5afda651" },
 };
 
 #endif /* EMBEDDED_CHECKSUMS_H */
