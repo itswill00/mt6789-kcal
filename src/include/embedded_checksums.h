6190a33df2a3d38353dbb60698a654be560d4b2a
itswill00 <itswill00@users.noreply.github.com>
2026-09-01 23:51:58 +0700

    fix: target Xiaomi miui_refresh_rate in system and secure settings tables

--- src/include/embedded_checksums.h
+++ src/include/embedded_checksums.h
@@ -9,7 +9,7 @@ typedef struct {
 static const file_checksum_t g_embedded_checksums[] = {
     { "service.sh", "9aac183e13a46a1fd2309db2a3aebe676ffbfe4cc44f070dc53fd9d7ce8e0419" },
     { "module.prop", "2cf91ed48d01f2fe73905e97ef21d277590256075c6d079d2ba91d847d109d9c" },
-    { "webroot/index.html", "deffd6ccebe9eeb8eb6e1c58473724202b6f9a4c951639e66f9bd609b393c551" },
+    { "webroot/index.html", "143cb6f757c09ce78809401810b506d0889d8bebc83db91dd9d59b4afb0c8446" },
 };
 
 #endif /* EMBEDDED_CHECKSUMS_H */
