dd3167806b4c92a48000062f1415c694837bed32
itswill00 <itswill00@users.noreply.github.com>
2026-09-01 23:42:40 +0700

    ui: format section labels in natural sentence-case typography

--- src/include/embedded_checksums.h
+++ src/include/embedded_checksums.h
@@ -9,7 +9,7 @@ typedef struct {
 static const file_checksum_t g_embedded_checksums[] = {
     { "service.sh", "9aac183e13a46a1fd2309db2a3aebe676ffbfe4cc44f070dc53fd9d7ce8e0419" },
     { "module.prop", "2cf91ed48d01f2fe73905e97ef21d277590256075c6d079d2ba91d847d109d9c" },
-    { "webroot/index.html", "0b38a3ff9365ffe2e43d185d45dae54ccb7d4c44c18e3f58deb84e3172f7f954" },
+    { "webroot/index.html", "8a07850712094e91faa7b2fb05b87192c974a153291e94a33734d5c7d1e08a77" },
 };
 
 #endif /* EMBEDDED_CHECKSUMS_H */
