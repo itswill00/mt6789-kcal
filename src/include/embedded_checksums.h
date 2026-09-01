4c23c4b206625faba2eb42b3e6543bf893d1b439
itswill00 <itswill00@users.noreply.github.com>
2026-09-01 23:33:34 +0700

    ui: redesign WebUI with HyperCore Material 3 Expressive theme and statusbar styling

--- src/include/embedded_checksums.h
+++ src/include/embedded_checksums.h
@@ -9,7 +9,7 @@ typedef struct {
 static const file_checksum_t g_embedded_checksums[] = {
     { "service.sh", "9aac183e13a46a1fd2309db2a3aebe676ffbfe4cc44f070dc53fd9d7ce8e0419" },
     { "module.prop", "2cf91ed48d01f2fe73905e97ef21d277590256075c6d079d2ba91d847d109d9c" },
-    { "webroot/index.html", "b68b081fe215dae5c5801bb454c0dc0479f8a62f58d5d8282d63d2044ce669a5" },
+    { "webroot/index.html", "aa57b793a6b781977a248748f77ef811af16d55f596fb0f9b459155dad0bdbe7" },
 };
 
 #endif /* EMBEDDED_CHECKSUMS_H */
